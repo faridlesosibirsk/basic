a56ca445c4a6a25dceee23ee3c62f25bb1660a32
Andrey Thunder <AndreyThunder1997@gmail.com>
2018-11-19 11:36:55 +0700

    modified:   controllers/SiteController.php 	new file:   models/ItsController.php 	new file:   models/core/Page.php 	renamed:    models/Paragraph.php -> models/core/Paragraph.php 	renamed:    models/Title.php -> models/core/Title.php 	new file:   views/layouts/mainits.php 	modified:   views/site/its.php

---
+++ models/core/Page.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\models\core;
+
+use yii\base\Model;
+
+class page extends Model {
+    public $Page = 'its';
+    public function getPage() {
+        return 'Page';
+        
+    }
+
+}
--- views/site/its.php
+++ views/site/its.php
@@ -1,16 +1,5 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
 ?>
-<?php $form = ActiveForm::begin(); ?>
-
-<?= $form->field($title, 'name') ?>
-
-<?= $form->field($paragraph, 'name') ?>
-
-<div class="form-group">
-    <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
-</div>
-
-<?php ActiveForm::end(); ?>
+<?= $model['Page']; ?>
